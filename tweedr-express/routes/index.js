52188fe1ea7aac13d39e5be70329e5b8afaa27c0
Gainor Bostwick <gainorbostwick@gmail.com>
2017-05-20 16:17:39 -0400

    readme, queries.js, routing and views

--- tweedr-express/routes/index.js
+++ tweedr-express/routes/index.js
@@ -2,15 +2,14 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db/queries');
 
-
-/*router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});*/
+router.get('/', function(req, res, next){
+  res.render('index', { title: "TweedApp" });
+});
 
 // GET FROM DATABASE
-router.get('/', db.getTweeds);
+router.get('/getTweeds', db.getTweeds);
 
 // POST TO DATABASE
-router.post('/', db.createTweed);
+router.post('/createTweed', db.createTweed);
 
 module.exports = router;
