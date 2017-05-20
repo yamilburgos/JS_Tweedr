68efdec2c7090f6519890988cd85d07309f55ac1
Gainor Bostwick <gainorbostwick@gmail.com>
2017-05-20 14:40:08 -0400

    first commit

---
+++ tweedr-react/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// IMPORT AXIOS
+import axios from 'axios';
+
+// IMPORT COMPONENTS
+//import ... from './components/...';
+//import ... from './components/...';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to Tweedr</h2>
+        </div>
+      </div>
+    );
+  }
+}
